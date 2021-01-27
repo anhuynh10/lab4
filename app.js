f16b7d2322a454139e0552d2a03c93ecbb35cfcb
An Huynh <anh005@ucsd.edu>
2021-01-27 10:34:16 -0800

    UP

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
 var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
@@ -39,6 +40,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
 // Example route
+app.get('/project/:name', project.viewProject);
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
